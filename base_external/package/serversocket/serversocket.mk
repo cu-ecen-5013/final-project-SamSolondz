84f7460be4ff614bc41e2e1b1c34c07f5cdfbe82
SamSolondz <samuel.solondz@colorado.edu>
2020-04-12 23:17:08 +0000

    Clean commit

---
+++ base_external/package/serversocket/serversocket.mk
@@ -0,0 +1,23 @@
+
+##############################################################
+#
+# SERVERSOCKET
+#
+##############################################################
+
+SERVERSOCKET_VERSION = '5ed11b947a349cc2a3704d00fac31babca3a4864'
+SERVERSOCKET_SITE = 'git@github.com:chwe3468/cu-ecen-5013-final-project-shared.git'
+SERVERSOCKET_SITE_METHOD = git
+
+
+define SERVERSOCKET_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) serversocket
+endef
+
+define SERVERSOCKET_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/serversocket $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/serversocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99serversocket
+endef
+
+
+$(eval $(generic-package))
